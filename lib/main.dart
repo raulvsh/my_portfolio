e8a02ef75740db4fe99f1b926fc507131bc824ac
Raúl Velasco <raulvsh@gmail.com>
2024-02-14 00:01:03 +0100

    API Coingecko implemented Prices retrieved Gains/Loses implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,129 @@
+//import 'dart:ffi';
+//import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Crypto Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" ifP you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
       ),
-      home: const MyHomePage(title: 'Mi Portafolio'),
+      home: CryptoList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CryptoList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CryptoListState createState() => _CryptoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CryptoListState extends State<CryptoList> {
+  List<Crypto> cryptos = [];
+  final List<double> preciosCompra = [
+      0.005330,
+      0.0000000000560,
+      0.00001367,
+      0.0004880,
+      0.00001040,
+      0.0001389,
+      0.0002114,
+      0.00000599
+    ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    // final response = await http.get(Uri.parse('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,litecoin'));
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&ids=aave,cardano,bittorrent,pancakeswap-token,polkadot,filecoin,the-graph,iota&order=id_asc'));
+    
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+
+      List<Crypto> fetchedCryptos =
+          data.map((crypto) => Crypto.fromJson(crypto)).toList();
+      for (var i = 0; i < fetchedCryptos.length; i++) {
+        print("shitcoin " + fetchedCryptos[i].symbol.toString());
+        print("precio de compra " + preciosCompra[i].toString());
+      }
+
+      setState(() {
+        cryptos = fetchedCryptos;
+      });
+    } else {
+      throw Exception('Error al cargar datos');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: Text('Mis shitcoins'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Has pulsado el texto $_counter veces:',
+      //body: Text('holaasda'),
+      body: ListView.builder(
+        itemCount: cryptos.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(cryptos[index].name),
+                        //subtitle: Text('${cryptos[index].symbol} - ${cryptos[index].name} - B${cryptos[index].price}' ),
+
+                        subtitle: Text('B${cryptos[index].price}'),
+                      ),
+                      Text(
+                          '${((cryptos[index].price / preciosCompra[index] - 1) * 100).toStringAsFixed(2)}%')
+                    ],
+                  ),
                 ),
-
-
-              ],
-            ),
-            const Text(
-              'Has pulsado el texto estas veces:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+              ),
+            ],
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Crypto {
+  final String name;
+  final String symbol;
+  final double price;
+
+  Crypto({
+    required this.name,
+    required this.symbol,
+    required this.price,
+  });
+
+  factory Crypto.fromJson(Map<String, dynamic> json) {
+    return Crypto(
+      name: json['name'],
+      symbol: json['symbol'],
+      price: json['current_price'].toDouble(),
     );
   }
 }
